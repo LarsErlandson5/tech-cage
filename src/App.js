669d7ba67bd6a4f655966df0aa235ace18076ea0
Lars Erlandson <larserlandson5@gmail.com>
2020-09-10 21:58:42 -0500

    fix accidental move of app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage'
+import CreateTicketPage from './CreateTicketPage'
+import ViewTicketPage from './ViewTicketPage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      displayHomePage: true,
+      displayTicketList: false,
+      displayCreateTicket: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {this.state.displayHomePage ? <HomePage /> : null}
+          {this.state.displayCreateTicket ? <CreateTicketPage /> : null}
+          {this.state.displayTicketList ? <ViewTicketPage /> : null}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
