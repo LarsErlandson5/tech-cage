4a6fd9a562b792312843ed401f479f0d84760c80
Lars Erlandson <larserlandson5@gmail.com>
2020-09-13 21:21:14 -0500

    add bootstrap validation to input form when creating a ticket

--- src/pages/CreateTicketPage/index.js
+++ src/pages/CreateTicketPage/index.js
@@ -5,6 +5,27 @@ import './index.css'
 import qr_code from '../../images/qr_code.png'
 
 export default class CreateTicketPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      validated: false
+    };
+  }
+
+  handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      this.props.history.push('/');
+    }
+
+   this.setState({
+     validated: true
+   });
+  };
+
   render() {
     return (
       <div>
@@ -15,7 +36,7 @@ export default class CreateTicketPage extends React.Component {
             </Col>
           </Row>
           <Row>
-            <Col style={{margin: 'auto'}}>
+            <Col style={{ margin: 'auto' }}>
               <h4>Create Ticket</h4>
             </Col>
             <Col xs={4}>
@@ -26,41 +47,45 @@ export default class CreateTicketPage extends React.Component {
           </Row>
           <Row>
             <Col>
-              <Form>
+              <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
                 <Form.Group controlId="formLine">
                   <Form.Label>Line</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
-                    <option>Select Line...</option>
+                  <Form.Control as="select" required>
+                    <option value=''>Select Line...</option>
                     <option>Range</option>
                     <option>Refrigeration</option>
                     <option>Ventilation</option>
                   </Form.Control>
+                  <Form.Control.Feedback type="invalid">Please select a Line.</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group controlId="formStation">
                   <Form.Label>Station</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
-                    <option>Select Station...</option>
+                  <Form.Control as="select" required>
+                    <option value='' >Select Station...</option>
                     <option>A1</option>
                     <option>B2</option>
                     <option>C3</option>
                   </Form.Control>
+                  <Form.Control.Feedback type="invalid">Please select a Station.</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group controlId="formPriority">
                   <Form.Label>Priority</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
-                    <option>Select Priority...</option>
+                  <Form.Control as="select" required>
+                    <option value=''>Select Priority...</option>
                     <option>Critical</option>
                     <option>High</option>
                     <option>Medium</option>
                     <option>Low</option>
-                  </Form.Control>
+                  </Form.Control
+                  ><Form.Control.Feedback type="invalid">Please select a Priority.</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group controlId="formDescription">
                   <Form.Label>Description</Form.Label>
-                  <Form.Control as="textarea" rows="5" />
+                  <Form.Control as="textarea" rows="5" required />
+                  <Form.Control.Feedback type="invalid">Please enter a description.</Form.Control.Feedback>
                 </Form.Group>
 
                 <Button variant="primary" type="submit">Submit</Button>
