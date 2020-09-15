e968656f579305424f376246dc55e82efba042e3
Lars Erlandson <larserlandson5@gmail.com>
2020-09-15 17:32:22 -0500

    ticket schema updated required items and added repairticket field

--- src/server/models/ticket.js
+++ src/server/models/ticket.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
 const ticketSchema = new mongoose.Schema({
-    line: String,
-    station: String,
-    priority: Number,
-    description: String,
+    line: {type: String, required: true},
+    station: {type: String, required: true},
+    priority: {type: Number, required: true},
+    description: {type: String, required: true},
+    repairdescription: {type: String},
+    createddate: { type: Date, required: true, default: Date }, 
+    lastupdateddate:{ type: Date, required: true, default: Date } 
   });
 
 const Ticket = mongoose.model('Ticket', ticketSchema);
