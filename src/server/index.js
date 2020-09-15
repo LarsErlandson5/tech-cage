7a5fcfd65bbe474d54a62ea10f4d577d55d448e6
Lars Erlandson <larserlandson5@gmail.com>
2020-09-15 08:09:02 -0500

    add routes to create and get tickets from mongo

--- src/server/index.js
+++ src/server/index.js
@@ -1,24 +1,37 @@
 const connectDB = require('./db');
-const express = require('express')
+const express = require('express');
 const Ticket = require('./models/ticket');
 
 connectDB();
 
-const app = express()
+const app = express();
 
-app.get('/api/tickets', (req, res) => {
-  res.send("hello world")
-})
+app.use(express.json());
 
-app.post('/api/createTicket', (req, res) => {
-  Ticket.create(req.body, function (err) {
-    if (err) throw new Error(err);
-    // saved!
-  })
+app.get('/api/health', (req, res) => {
+  res.sendStatus(200);
+});
 
-  res.sendStatus(200)
-})
+app.get('/api/getTickets', async (req, res, next) => {
+  await Ticket.find({}, function (err, docs) {
+    if (err) {
+      next(err);
+    } else {
+      res.send(docs);
+    }
+  });
+});
+
+app.post('/api/createTicket', (req, res, next) => {
+  Ticket.create(req.body, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
-)
+);
