753baf92998d184227e52d3e8ee7091958765047
Lars Erlandson <larserlandson5@gmail.com>
2020-09-14 21:27:40 -0500

    added create ticket post

--- src/server/index.js
+++ src/server/index.js
@@ -9,8 +9,12 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.get('/api/tickets', async (req, res) => {
+app.get('/api/tickets', (req, res) => {
     res.send("hello world")
+})
+app.post('/api/createTicket', (req, res) => {
+  console.log('sent ticket', req.body )
+  res.sendStatus(200)
 })
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
