9df52211f215d4cefcad011636654f51ff90781b
Lars Erlandson <larserlandson5@gmail.com>
2020-09-22 23:18:08 -0500

    cleanup console warnings and messages

--- src/app/pages/Landing/LoginForm/index.js
+++ src/app/pages/Landing/LoginForm/index.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
+import {
+  Container,
+  Row,
+  Col
+} from 'react-bootstrap'
 import Axios from 'axios';
 import { useCookies } from 'react-cookie'
 
 function LoginForm() {
-  const [cookies, setCookie] = useCookies(['techCage'])
+  const [cookies, setCookie] = useCookies(['techCage']) // eslint-disable-line
   const [email, setEmail] = useState();
   const [password, setPassword] = useState()
 
   const handleSubmit = (event) => {
-
     event.preventDefault();
-
 
     Axios.post('http://localhost:3001/api/validate', {
       email: email,
       password: password
     })
       .then(response => {
-        console.log(response, 'response')
-        if (response.data == true) {
-          //send to home page
+        if (response.data === true) {
           setCookie('techCage', email, { path: '/' })
           window.location.href = '/homepage';
         } else {
@@ -40,28 +41,34 @@ function LoginForm() {
   }
 
   return (
-    <div className="loginWrap">
-      <div className="input">
-        <form className="input" onSubmit={handleSubmit}>
-          <div>
-            <h5>Login</h5>
-          </div>
-          <div>
-            <input type="text" name="email" placeholder="Enter your email" onChange={(e) => onChangeEmail(e)} required />
-          </div>
-          <div>
-            <input type="password" name="password" placeholder="Enter your Access Code" onChange={(e) => onChangePassword(e)} required />
-          </div>
-          <div>
-            <div>
-              <span>
-                <button className="submit" type="submit">Authorize</button>
-              </span>
+    <Container>
+      <Row>
+        <Col>
+          <div className="loginWrap">
+            <div className="input">
+              <form className="input" onSubmit={handleSubmit}>
+                <div>
+                  <h5>Login</h5>
+                </div>
+                <div>
+                  <input type="text" name="email" placeholder="Enter your email" onChange={(e) => onChangeEmail(e)} required />
+                </div>
+                <div>
+                  <input type="password" name="password" placeholder="Enter your Access Code" onChange={(e) => onChangePassword(e)} required />
+                </div>
+                <div>
+                  <div>
+                    <span>
+                      <button className="submit" type="submit">Authorize</button>
+                    </span>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-        </form>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/app/pages/TicketDetailsPage/index.js
+++ src/app/pages/TicketDetailsPage/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import ViewTicket from './ViewTicket'
-import EditTicket from './EditTicket'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import {
+  Alert,
+  Form,
+  Container,
+  Row,
+  Col,
+  Button
+} from 'react-bootstrap'
+import qr_code from '../../images/qr_code.png'
 
 export default class TicketDetailsPage extends React.Component {
   constructor(props) {
@@ -11,13 +20,76 @@ export default class TicketDetailsPage extends React.Component {
     }
   }
 
+  componentDidMount = () => {
+    this.getTicket(new URLSearchParams(window.location.search).get('id'));
+  }
+
+  getTicket = async (id) => {
+    let ticket = await axios.get('http://localhost:3001/api/getTicket', {
+      params: {
+        id
+      }
+    });
+
+    this.setState({ ticket: ticket.data });
+  }
+
   render() {
-    return (
-      <div>
-        {/* TODO: use ternary operator (single-line if-statement) to decide if edit or view */}
-        <ViewTicket ticket={this.state.ticket} />
-        <EditTicket ticket={this.state.ticket} />
+    return this.state.ticket.hasOwnProperty('station')
+      ? <div>
+        <Container>
+          <Row>
+            <Col>
+              <Link to="/TicketList">&lt; Back</Link>
+            </Col>
+          </Row>
+          <Row>
+            <Col style={{ margin: 'auto' }}>
+              <h4>Ticket Details</h4>
+            </Col>
+            <Col xs={4}>
+              <Button variant="outline-secondary" id="qr_code">
+                <img src={qr_code} alt={qr_code} />
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Alert variant='danger'>{this.state.ticket.station}</Alert>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form>
+                <Form.Group controlId="formDescription">
+                  <Form.Label>Description:</Form.Label>
+                  <Form.Control as="textarea" rows="5" disabled value={this.state.ticket.description} />
+                </Form.Group>
+
+                <Form.Group controlId="formRepair">
+                  <Form.Label>Repair</Form.Label>
+                  <Form.Control as="textarea" rows="5" />
+                </Form.Group>
+
+                <Container>
+                  <Row>
+                    <Col>
+                      <Link to="/CreateTicket">
+                        <Button variant="primary" size="lg">Notify TE</Button>
+                      </Link>
+                    </Col>
+                    <Col>
+                      <Link to="/Queue">
+                        <Button variant="primary" size="lg">Complete</Button>
+                      </Link>
+                    </Col>
+                  </Row>
+                </Container>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
       </div>
-    )
+      : null
   }
 }
